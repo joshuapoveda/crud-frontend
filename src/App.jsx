3a5d22e669aa59128db62d5f49cc6507a2387a1d
Joshua Poveda <joshuapoveda9@gmail.com>
2023-07-17 01:36:09 -0400

    added axios and first call to local backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+import axios from "axios"
 import "./App.css";
 
 function App() {
+  const [notes, setNotes] = useState(null)
+
+  useEffect(()=>{
+    fetchNotes()
+  },[])
+
+  const fetchNotes = async () => {
+    const res = await axios.get("http://localhost:3000/notes")
+    setNotes(res.data.notes)
+  }
+  console.log(notes)
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className="text-3xl text-center">Hello</h1>
+      <div className="border-4 border-pink-700 h-32">
+        {notes && notes.map(note => {
+          return(
+          <div key={note._id}>
+            <h3>{note.title}</h3>
+          </div>)
+        })}
+      </div>
     </div>
   );
 }
